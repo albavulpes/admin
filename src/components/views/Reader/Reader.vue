f82ebba0bc03e9ac79b23cc5e2025980b18c84d4
cypherix93 <bikram.ghosh93@gmail.com>
2018-11-19 00:25:12 -0500

    Removed unused stuff

--- src/components/views/Reader/Reader.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" src="./Reader.ts"></script>
-
-<template>
-    <div class="ReaderComponent container-fluid">
-        <ReaderPages></ReaderPages>
-
-        <ReaderControls class="reader-controls-container"></ReaderControls>
-    </div>
-</template>
