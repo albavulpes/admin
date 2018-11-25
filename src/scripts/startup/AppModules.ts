79bf1d60fa39045bff9809e9870ec324e0fbca2b
cypherix93 <bikram.ghosh93@gmail.com>
2018-11-25 11:20:56 -0500

    Added toast setings

--- src/scripts/startup/AppModules.ts
+++ src/scripts/startup/AppModules.ts
@@ -6,5 +6,10 @@ Vue.use(UiCore, {
     http: {
         baseURL: AppConfig.ApiPath,
         withCredentials: true
+    },
+    toast: {
+        position: 'bottomCenter',
+        transitionOut: 'fadeOutDown',
+        timeout: 3000
     }
 });
