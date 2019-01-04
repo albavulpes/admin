506d446dcb9ae726093206e578ce69da12a16f26
cypherix93 <bikram.ghosh93@gmail.com>
2019-01-03 20:38:22 -0500

    Updated the edit form a bit

--- src/components/views/Comics/ComicViewer/ComicViewer.vue
+++ src/components/views/Comics/ComicViewer/ComicViewer.vue
@@ -3,44 +3,67 @@
 
 <template>
     <div class="ComicViewerComponent container" v-if="Comic">
-        <div>
-            <router-link class="btn btn-outline-primary" :to="{name: 'comics'}">
-                <i class="mdi mdi-arrow-left"></i>
-                Back to Comics
-            </router-link>
-        </div>
-
-        <hr>
-
-        <div class="row comicEditor" :class="{editMode: !IsCreateMode}">
-            <div class="col-12 col-md-5 col-lg-4">
-                <img class="img-fluid mb-4" :src="Comic.CoverImage.FullSize" :alt="Comic.Title">
+        <form @submit.prevent="SubmitForm">
+            <div class="d-flex justify-content-between">
+                <div>
+                    <router-link class="btn btn-outline-primary" :to="{name: 'comics'}">
+                        <i class="mdi mdi-arrow-left"></i>
+                        Back to Comics
+                    </router-link>
+                </div>
+                <div>
+                    <button type="button" class="btn btn-outline-secondary">
+                        <i class="mdi mdi-publish"></i>
+                        Publish Comic
+                    </button>
+                    <button type="submit" class="btn btn-primary">
+                        <i class="mdi mdi-content-save-outline"></i>
+                        Save Comic
+                    </button>
+                </div>
             </div>
-            <div class="col">
-                <form @submit.prevent="SubmitForm">
+
+            <hr>
+
+            <div class="row comicEditor">
+                <div class="col-xs-12 col-md coverContainer">
+                    <img class="img-fluid mb-4" :src="Comic.CoverImage.FullSize" :alt="Comic.Title">
+                </div>
+                <div class="col">
                     <div class="form-group">
-                        <label>Title</label>
-                        <input type="text" class="form-control form-control-lg editableField" placeholder="Title for your comic" v-model="Comic.Title">
-                    </div>
-                    <div class="form-group">
-                        <label>Author</label>
-                        <input type="text" class="form-control editableField" placeholder="Author of your comic" v-model="Comic.Author">
-                    </div>
-                    <div class="form-group">
-                        <label>Description</label>
-                        <textarea class="form-control editableField" placeholder="Short description of the comic" rows="4" v-model="Comic.Description"></textarea>
+                        <input type="text" class="form-control-plaintext form-control-lg" placeholder="Title for your comic" readonly v-model="Comic.Title">
                     </div>
 
-                    <div class="row justify-content-end">
-                        <div class="col-12 col-lg-6">
-                            <button type="submit" class="btn btn-block btn-primary">
-                                <i class="mdi mdi-content-save-outline"></i>
-                                Save Comic
-                            </button>
+                    <hr>
+
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Author</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control-plaintext" placeholder="Author of your comic" readonly v-model="Comic.Author">
                         </div>
                     </div>
-                </form>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Release Date</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control-plaintext" placeholder="Author of your comic" readonly :value="Comic.ReleaseDate | moment('LL')">
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    <div class="form-group">
+                        <label>Description</label>
+                        <textarea class="form-control" placeholder="Short description of the comic" rows="4" v-model="Comic.Description"></textarea>
+                    </div>
+
+
+                    <div class="mt-5">
+                        <h2>Arcs</h2>
+                    </div>
+
+
+                </div>
             </div>
-        </div>
+        </form>
     </div>
 </template>
