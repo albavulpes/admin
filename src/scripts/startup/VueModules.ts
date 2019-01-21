a234f648359f105cd31dc7ebff517c5183e74ece
cypherix93 <bikram.ghosh93@gmail.com>
2019-01-20 21:03:47 -0500

    Using bootstrap vue

--- src/scripts/startup/VueModules.ts
+++ src/scripts/startup/VueModules.ts
@@ -3,6 +3,7 @@ import Vuex from 'vuex';
 import VueRouter from 'vue-router';
 import VueMeta from 'vue-meta'
 
+import BootstrapVue from 'bootstrap-vue'
 import Vue2Filters from 'vue2-filters';
 import VueMoment from 'vue-moment';
 
@@ -12,5 +13,6 @@ Vue.use(VueRouter);
 Vue.use(VueMeta);
 
 // Plugins
+Vue.use(BootstrapVue);
 Vue.use(Vue2Filters);
 Vue.use(VueMoment);
