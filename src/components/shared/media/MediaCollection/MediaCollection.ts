4918106f4c48df450d9272c5de52676d37fa6e43
cypherix93 <bikram.ghosh93@gmail.com>
2019-02-03 00:01:06 -0500

    Media collection component

---
+++ src/components/shared/media/MediaCollection/MediaCollection.ts
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+import {Route} from 'vue-router';
+
+import MediaCard from '../../MediaCard/MediaCard.vue';
+import MediaAddButton from '../../MediaAddButton/MediaAddButton.vue';
+
+@Component({
+    components: {
+        MediaCard,
+        MediaAddButton,
+    }
+})
+export default class extends Vue {
+
+    @Prop()
+    MediaItems: MediaContentCollection[];
+
+    @Prop()
+    MediaDetailsRoute: (item: MediaContentCollection) => Route;
+
+    @Prop()
+    AddMediaRoute: Route;
+}
---
+++ src/components/shared/media/MediaCollection/MediaCollection.vue
@@ -0,0 +1,29 @@
+<script lang="ts" src="./MediaCollection.ts"></script>
+
+<template>
+    <div class="MediaCollectionComponent">
+        <div class="row">
+            <div class="col-4 col-lg-3" v-for="item in MediaItems">
+                <router-link class="text-decoration-none" :to="MediaDetailsRoute(item)">
+                    <MediaCard :Image="item.CoverImage.FullSize" :ImageAlt="item.Title">
+                        <template slot="title">
+                            <slot name="title" :item="item">
+                                {{item.Title}}
+                            </slot>
+                        </template>
+                        <template slot="content">
+                            <slot name="content" :item="item"></slot>
+                        </template>
+                    </MediaCard>
+                </router-link>
+            </div>
+            <div class="col-4 col-lg-3">
+                <router-link class="text-decoration-none" :to="AddMediaRoute">
+                    <MediaAddButton>
+                        <slot name="add-button-content"></slot>
+                    </MediaAddButton>
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
