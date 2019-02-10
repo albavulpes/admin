07f20265d8ae1fc361c680b4cc18957cc92ea0eb
cypherix93 <bikram.ghosh93@gmail.com>
2019-02-10 13:35:58 -0500

    Body slot on media card

--- src/components/shared/MediaCard/MediaCard.vue
+++ src/components/shared/MediaCard/MediaCard.vue
@@ -5,15 +5,17 @@
     <button type="button" class="MediaCardComponent btn btn-block card d-flex flex-column" @click="CardClick">
         <div class="card-img-top" :style="{'background-image': `url('${Image}')`}"></div>
 
-        <div class="card-body text-left" v-if="$slots.title || $slots.content">
-            <h5 class="card-title" v-if="$slots.title">
-                <slot name="title"></slot>
-            </h5>
-            <p class="h6 card-text" v-if="$slots.content">
-                <small>
-                    <slot name="content"></slot>
-                </small>
-            </p>
+        <div class="card-body text-left" v-if="$slots.body || $slots.title || $slots.content">
+            <slot name="body">
+                <h5 class="card-title" v-if="$slots.title">
+                    <slot name="title"></slot>
+                </h5>
+                <p class="h6 card-text" v-if="$slots.content">
+                    <small>
+                        <slot name="content"></slot>
+                    </small>
+                </p>
+            </slot>
         </div>
     </button>
 </template>
