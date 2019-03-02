e45a76adb69475d27bbfe6a3bc908cd16eb44356
cypherix93 <bikram.ghosh93@gmail.com>
2019-03-02 15:28:20 -0500

    Reorder pages!!!

--- src/components/views/Chapters/ChapterViewer/ChapterPages/ChapterPages.ts
+++ src/components/views/Chapters/ChapterViewer/ChapterPages/ChapterPages.ts
@@ -3,8 +3,9 @@ import {Component, Prop} from 'vue-property-decorator';
 import {Require} from '@albavulpes/ui-core/dist/di';
 import {HttpService} from '@albavulpes/ui-core/dist/services/app/HttpService';
 import {ManageChapterStore} from '../../../../../scripts/stores/ManageChapterStore';
+import {ToastService} from '@albavulpes/ui-core/dist/services/ui/ToastService';
 
-import Draggable from 'vuedraggable';
+import Draggable, {DragChangeEvent} from 'vuedraggable';
 
 import MediaCard from '../../../../shared/media/MediaCard/MediaCard.vue';
 import MediaAddButton from '../../../../shared/media/MediaAddButton/MediaAddButton.vue';
@@ -22,12 +23,15 @@ export default class extends Vue {
     HttpService: HttpService;
 
     @Require()
+    ToastService: ToastService;
+
+    @Require()
     ManageChapterStore: ManageChapterStore;
 
     Pages: Page[] = [];
 
     async created() {
-        this.Pages = await this.HttpService.api.pages.getAll(this.Chapter.Id);
+        await this.FetchPages();
     }
 
     get Chapter() {
@@ -38,11 +42,29 @@ export default class extends Vue {
         return {
             filter: '.nodrag',
             animation: 250
-        }
+        };
     }
 
-    async OnPageOrderChange() {
+    async FetchPages() {
+        this.Pages = await this.HttpService.api.pages.getAll(this.Chapter.Id);
+    }
 
+    async OnPageOrderChange(dragEvent: DragChangeEvent<Page>) {
+        if (!dragEvent.moved) {
+            return;
+        }
+
+        const movedEvent = dragEvent.moved;
+        const page = movedEvent.element;
+
+        try {
+            await this.HttpService.api.pages.reorder(page.Id, movedEvent.newIndex);
+
+            await this.FetchPages();
+        }
+        catch (error) {
+            this.ToastService.error(error);
+        }
     }
 
     async AddPages() {
