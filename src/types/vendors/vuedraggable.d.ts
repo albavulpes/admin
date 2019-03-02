cf913d06ec5e502bfede2bfde83050bb61173f28
cypherix93 <bikram.ghosh93@gmail.com>
2019-03-02 15:28:09 -0500

    vuedraggable types

--- src/types/vendors/vuedraggable.d.ts
+++ src/types/vendors/vuedraggable.d.ts
@@ -3,4 +3,24 @@ declare module 'vuedraggable' {
 
     export default class extends Vue {
     }
+
+    interface DragChangeEvent<T> {
+        moved: DragMovedEvent<T>;
+    }
+
+    interface DragAddedEvent<T> {
+        element: T;
+        newIndex: number;
+    }
+
+    interface DragRemovedEvent<T> {
+        element: T;
+        oldIndex: number;
+    }
+
+    interface DragMovedEvent<T> {
+        element: T;
+        oldIndex: number;
+        newIndex: number;
+    }
 }
